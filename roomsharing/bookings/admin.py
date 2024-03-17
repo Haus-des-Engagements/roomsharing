8b7099fda03cfd375543e7943b1d578e33458789
finnus <40977905+finnus@users.noreply.github.com>
2024-03-17 15:10:11 +0100

    feat: use DateTimeRange as field and add constraint on not booking twice at the same time (Postgres)

--- roomsharing/bookings/admin.py
+++ roomsharing/bookings/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin  # Register your models here.
+
+from .models import Bookings
+from .models import Rooms
+
+
+@admin.register(Rooms)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Bookings)
+class BookingAdmin(admin.ModelAdmin):
+    pass
--- roomsharing/bookings/models.py
+++ roomsharing/bookings/models.py
@@ -1,3 +1,6 @@
+from django.contrib.postgres.constraints import ExclusionConstraint
+from django.contrib.postgres.fields import DateTimeRangeField
+from django.contrib.postgres.fields import RangeOperators
 from django.db import models
 
 
@@ -9,9 +12,7 @@ class Rooms(models.Model):
 
 
 class Bookings(models.Model):
-    date = models.DateField()
-    start_time = models.DateTimeField("Start time")
-    end_time = models.DateTimeField("End time")
+    timespan = DateTimeRangeField("Date Time Range", default_bounds="()")
     room = models.ForeignKey(
         Rooms,
         on_delete=models.PROTECT,
@@ -19,5 +20,16 @@ class Bookings(models.Model):
         related_query_name="booking_of_room",
     )
 
+    class Meta:
+        constraints = [
+            ExclusionConstraint(
+                name="exclude_overlapping_reservations",
+                expressions=[
+                    ("timespan", RangeOperators.OVERLAPS),
+                    ("room", RangeOperators.EQUAL),
+                ],
+            ),
+        ]
+
     def __str__(self):
-        return self.id
+        return str(self.id)
